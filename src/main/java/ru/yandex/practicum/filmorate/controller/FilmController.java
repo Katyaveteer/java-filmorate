bafed10b98a0ad9acaadc7d7c4b6836078375fec
Katya <katyaveteer@yandex.ru>
2025-06-23 16:22:08 +0300

    1 реализация

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,98 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+
+    private final Map<Long, Film> films = new HashMap<>();
+    private long nextId = 1;
+
+    @GetMapping
+    public Collection<Film> findAll() {
+        return films.values();
+    }
+
+    @PostMapping
+    public Film create(@RequestBody Film film) {
+
+        //проверка всех критериев
+        //1. Название не может быть пустым
+        if(film.getName() == null || film.getName().isBlank()) {
+            String error = "Название не может быть пустым";
+            log.error("Ошибка создания фильма: {}", error);
+            throw new ValidationException(error);
+        }
+
+        //2. Максимальнаядлна описания - 200 символов
+        if (film.getDescription().length() > 200) {
+            String error = "\"Максимальная длна описания - 200 символов\")";
+            log.error("Ошибка создания фильма: {}", error);
+            throw new ValidationException(error);
+        }
+
+        //3. Дата релиза - не раньше 28 декабря 1895 года
+        if (film.getReleaseDate().isBefore(LocalDate.of(1895, Month.DECEMBER,28))) {
+            String error = "Дата релиза - не раньше 28 декабря 1895 года";
+            log.error("Ошибка создания фильма: {}", error);
+            throw new ValidationException(error);
+        }
+
+        //4. Продолжительность фильма должна быть положительным числом
+        if (film.getDuration().isNegative()) {
+            String error = "Продолжительность фильма должна быть положительным числом";
+            log.error("Ошибка создания фильма: {}", error);
+            throw new ValidationException(error);
+
+        }
+        film.setId(nextId++);
+        films.put(film.getId(),film);
+        return film;
+
+    }
+
+    @PutMapping
+    public Film update(@RequestBody Film newFilm) {
+        if (newFilm.getId() == null) {
+            String error = "Id должен быть указан";
+            log.error("Ошибка обновления фильма: {}", error);
+            throw new ValidationException(error);
+        }
+        if (films.containsKey(newFilm.getId())) {
+            Film oldFilm = films.get(newFilm.getId());
+            if (newFilm.getName() != null) {
+                oldFilm.setName(newFilm.getName());
+            }
+
+            if (newFilm.getDescription().length() <= 200) {
+                oldFilm.setDescription(newFilm.getDescription());
+            }
+            if (newFilm.getDuration().isPositive()) {
+                oldFilm.setDuration(newFilm.getDuration());
+
+            }
+            if (newFilm.getReleaseDate().isAfter(LocalDate.of(1895, Month.DECEMBER,28))) {
+                oldFilm.setReleaseDate(newFilm.getReleaseDate());
+            }
+
+            return oldFilm;
+        }
+        String error = "Пост с id = " + newFilm.getId() + " не найден";
+        log.error("Ошибка обновления фильма: {}", error);
+        throw new NotFoundException(error);
+    }
+
 }
+
---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,104 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final Map<Long, User> users = new HashMap<>();
+    private long nextId = 1;
+
+    @GetMapping
+    public Collection<User> findAll() {
+        return users.values();
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+
+        //проверка всех критериев
+        //1. Электронная почта не может быть пустой и должна содержать символ @;
+        if (user.getEmail() == null || user.getEmail().isBlank() || user.getEmail().contains("@")) {
+            String error = "Электронная почта не может быть пустой и должна содержать символ @";
+            log.error("Ошибка создания пользователя: {}", error);
+            throw new ValidationException(error);
+        }
+
+        //2. Логин не может быть пустым и содержать пробелы;
+        if (user.getLogin() == null || user.getLogin().isBlank()) {
+            String error = "Логин не может быть пустым и содержать пробелы";
+            log.error("Ошибка создания пользователя: {}", error);
+            throw new ValidationException(error);
+        }
+
+        //3. Имя для отображения может быть пустым — в таком случае будет использован логин;
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
+
+        //4. Дата рождения не может быть в будущем.
+        if (user.getBirthday().isAfter(LocalDate.now())) {
+            String error = "Дата рождения не может быть в будущем";
+            log.error("Ошибка создания пользователя: {}", error);
+            throw new ValidationException(error);
+
+        }
+        user.setId(nextId++);
+        users.put(user.getId(), user);
+        return user;
+
+    }
+
+    @PutMapping
+    public User update(@RequestBody User newUser) {
+        if (newUser.getId() == null) {
+            String error = "Id должен быть указан";
+            log.error("Ошибка обновления пользователя: {}", error);
+            throw new ValidationException(error);
+        }
+        User existingUser = users.get(newUser.getId());
+        if (existingUser == null) {
+            String error = "Пользователь с id = " + newUser.getId() + " не найден";
+            log.error("Ошибка обновления пользователя: {}", error);
+            throw new NotFoundException(error);
+        }
+        if (newUser.getEmail() != null && !newUser.getEmail().isEmpty()) {
+            for (User user : users.values()) {
+                if (!user.getId().equals(newUser.getId()) &&
+                        user.getEmail().equals(newUser.getEmail())) {
+                    String error = "Этот имейл уже используется";
+                    log.error("Ошибка обновления пользователя: {}", error);
+                    throw new ValidationException(error);
+                }
+            }
+            existingUser.setEmail(newUser.getEmail());
+        }
+        if (newUser.getBirthday() != null && !newUser.getBirthday().isAfter(LocalDate.now())) {
+            existingUser.setBirthday(newUser.getBirthday());
+        }
+        if (newUser.getName() != null) {
+            existingUser.setName(newUser.getName());
+        } else {
+            existingUser.setName(newUser.getEmail());
+        }
+
+        if (newUser.getLogin() != null) {
+            existingUser.setLogin(newUser.getLogin());
+        }
+        return existingUser;
+
+
+    }
+}
+
