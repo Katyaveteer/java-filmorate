e3fa9ace293305d6af265c4676b1b33599182095
Katya <katyaveteer@yandex.ru>
2025-06-24 15:22:52 +0300

    Создан каркас проекта

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,20 +1,12 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-
 import java.time.Duration;
-
 import java.time.LocalDate;
 
-/**
- * Film.
- */
+
 @Data
 @EqualsAndHashCode(of = {"id"})
 public class Film {
